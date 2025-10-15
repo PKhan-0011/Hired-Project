626413ba0a7ae470e7993e714d3fd9136c2f4299
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-10-15 20:58:18 +0530

    code commit

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,23 @@
 
 const LandingPage = () => {
   return (
-    <div>
-        LandingPage
-    </div>
+   <main>
+     <section className="flex flex-col gap-3">
+         <h1 className="text-8xl max-w-5xl mx-auto font-extrabold tracking-tight flex flex-col gap-3">Find Your Dream job <span className="flex flex-row pr-15 justify-center gap-5">and get <img src="logo (3).png" alt="# hiredImage" className="w-50 h-30"/></span></h1>
+          <p className="text-center text-2xl font-semibold text-neutral-400">Your career journey starts here — or find the perfect fit for your team. </p>
+     </section>
+       <div>
+           {/* buttons */}
+            {/*carousel */}
+       </div>
+
+       {/* banner */}
+        <section>
+             {/* cards*/}
+        </section>
+
+        {/* Accordian */ }
+   </main>
   )
 }
 
